c3c302b3040c3b6337e25b7b88d6a8ef279bc727
davidsakh <davidsakh@gmail.com>
2017-12-15 11:41:22 -0500

    upgraded project to dotnet core 2.0

--- EmployeeBenefits/Startup.cs
+++ EmployeeBenefits/Startup.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -12,7 +8,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace EmployeeBenefits
 {
-    public class Startup
+	public class Startup
     {
         public Startup(IHostingEnvironment env)
         {
